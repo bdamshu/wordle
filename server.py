f72ceab4318e8e831c6b0123400657abc9df6643
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2022-11-17 10:21:46 -0800

    Add game engine

---
+++ server.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+class Wordle:
+    def __init__(self, word):
+        assert len(word) == 5
+        self.word = word
+        self.chars = set(word)
+
+    def evaluate(self, guess):
+        assert len(guess) == 5
+        evaluation = ['x'] * 5
+        counts = Counter(self.word) # To ensure that the same letter is not used for G and Y
+
+        # Look for greens first, so as not to reuse same letter for both green and yello
+        for idx in range(5):
+            guess_ltr = guess[idx]
+            word_ltr = self.word[idx]
+
+            if guess_ltr == word_ltr:
+                evaluation[idx] = 'G'
+                counts[word_ltr] -= 1
+
+        # Now look for yellows
+        for idx in range(5):
+            guess_ltr = guess[idx]
+            if evaluation[idx]!='G' and guess[idx] in self.chars and counts[guess_ltr] :
+                evaluation[idx] = 'Y'
+                counts[guess[idx]]
+
+        eval_str = ''.join(evaluation)
+        return eval_str
